f86264a8b513395e6753ce01ce722ffdc5d150cb
Surender <surenderdargan@gmail.com>
2019-05-02 18:25:18 -0700

    changed by architect B

--- src/test/java/TestingWithMaven/SeleniumTest.java
+++ src/test/java/TestingWithMaven/SeleniumTest.java
@@ -4,7 +4,11 @@ import org.testng.annotations.Test;
 
 public class SeleniumTest {
 	@Test
-	public void Method5() {
+	public void Method1() {
+		System.out.println("Method5");
+	}
+	@Test
+	public void Method2() {
 		System.out.println("Method5");
 	}
 	
